2be9ef67dd286df1c11529932fa93acf756bc198
yuva2g <33248860+yuva2g@users.noreply.github.com>
2018-04-13 09:21:30 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
-      System.out.println("Qick Change");
-      System.out.println("Anil bidari");
+      System.out.println("Quick Change");
+      System.out.println(" N Yuvaraj ");
    }
 }
